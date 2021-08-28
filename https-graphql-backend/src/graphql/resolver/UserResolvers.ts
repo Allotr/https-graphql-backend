b16dd79e9fee562d4666e1ecadef25cba7c2c719
Rafael Pernil <rafaelpernil@uma.es>
2021-08-28 17:08:22 +0200

    fix: Upgraded dependencies + new resolvers

--- https-graphql-backend/src/graphql/resolver/UserResolvers.ts
+++ https-graphql-backend/src/graphql/resolver/UserResolvers.ts
@@ -1,25 +1,30 @@
 
-import { Resolvers, ResultDbObject } from "allotr-graphql-schema-types";
-import { RedisSingleton } from "../../utils/redis-singleton";
+import { Resolvers, ResultDbObject, UserDbObject } from "allotr-graphql-schema-types";
 import { MongoDBSingleton } from "../../utils/mongodb-singleton";
-
+import { RedisSingleton } from "../../utils/redis-singleton";
 
 export const UserResolvers: Resolvers = {
-    Query: {
-        results: async () => {
+  Query: {
+    results: async () => {
 
-            // TEST REDIS PUBSUB
-            RedisSingleton.getInstance().pubsub.publish('something_changed', { newUpdate: { result: "Otra respuesta..." } })
+        // TEST REDIS PUBSUB
+        RedisSingleton.getInstance().pubsub.publish('something_changed', { newUpdate: { result: "Otra respuesta..." } })
 
-            const db = await MongoDBSingleton.getInstance().db;
-            // Find all results
-            const dbOutput = await db.collection<ResultDbObject>('results').find()
+        const db = await MongoDBSingleton.getInstance().db;
+        // Find all results
+        const dbOutput = await db.collection<ResultDbObject>('results').find()
 
-            if (dbOutput == null) {
-                return [];
-            }
-          
-            return dbOutput.toArray() || [];
+        if (dbOutput == null) {
+            return [];
         }
+
+        return dbOutput.toArray() || [];
+    },
+    currentUser: (parent, args, context) => context.user
+  },
+  Subscription: {
+    newUpdate: {
+      subscribe: () => RedisSingleton.getInstance().pubsub.asyncIterator('something_changed'),
     }
+  }
 }
