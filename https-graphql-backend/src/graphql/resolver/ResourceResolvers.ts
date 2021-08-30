6e456b5e4b758a11b58384839d6a2cfb12ce7464
Rafael Pernil <rafaelpernil@uma.es>
2021-08-30 04:51:15 +0200

    fix: Rollback to working code

--- https-graphql-backend/src/graphql/resolver/ResourceResolvers.ts
+++ https-graphql-backend/src/graphql/resolver/ResourceResolvers.ts
@@ -18,7 +18,7 @@ export const ResourceResolvers: Resolvers = {
                 }
             };
 
-            const options = {
+            const options: any = {
                 projection: {
                     "tickets.$": 1 ,
                     name: 1 ,
