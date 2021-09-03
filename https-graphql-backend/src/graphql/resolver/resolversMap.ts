650912fbe0648822289939d25af44de85393b48a
Rafael Pernil <rafaelpernil@uma.es>
2021-09-03 17:49:09 +0200

    fix: Removed unused files

--- https-graphql-backend/src/graphql/resolver/resolversMap.ts
+++
@@ -1,8 +0,0 @@
-import { IResolvers } from "@graphql-tools/utils";
-import { merge } from "lodash";
-import { UserResolvers } from "./UserResolvers";
-import { ResourceResolvers } from "./ResourceResolvers";
-
-const resolverMap: IResolvers = merge(UserResolvers, ResourceResolvers);
-export default resolverMap;
-
