ae32e9dd19e2df13c14fc316f649fb2d0f1474e0
rafaelpernil2 <mysterysynthman@gmail.com>
2023-08-12 23:12:43 +0200

    fix: scripts

--- deployProd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -rf ./template
-faas-cli template pull https://github.com/rafaelpernil2/openfaas-template-node-typescript-express
-faas-cli deploy -f ./https-graphql-backend-prod.yml
